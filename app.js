f4ddc61b1e8f4a2cc3ad682389beddd3dbcb7be7
Gabriel <gbordesc@uft.edu>
2020-03-01 18:42:25 -0300

    Hello world in express

---
+++ app.js
@@ -0,0 +1,11 @@
+//imports
+const express=require('express');
+const app=express();
+const port=3000;
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+  });
+
+
+app.listen(port,console.log('Servidor funcionando en el puerto '+port))
